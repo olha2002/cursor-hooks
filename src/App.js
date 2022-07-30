30291658347dab9cc8b7356c2fa8c793605d0883
Olha Orach <olhaorach2002@gmail.com>
2022-07-30 22:07:16 +0100

    Did hw 16.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import "./App.css";
+import Contacts from "./components/Contacts/Contacts.js";
+import "./styles/contacts.css";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1 className="title">Homework #16</h1>
+      <Contacts />
+    </div>
   );
 }
 
